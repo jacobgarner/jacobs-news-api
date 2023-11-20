cf1a46713a482d16febc52762862ee68c4dc9ac0
jacob <jacobabgarner@gmail.com>
2023-11-20 15:00:31 +0000

    adding get api endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -28,10 +28,24 @@ describe("/api/topics", () => {
   });
 });
 
-describe("/api/doesNotExist",()=>{
-  it("Invalid URL returns 404",()=>{
+describe("/api/doesNotExist", () => {
+  it("Invalid URL returns 404", () => {
+    return request(app).get("/api/doesNotExist").expect(404);
+  });
+});
+
+describe("/api", () => {
+  it("GET responds with status code 200", () => {
+    return request(app).get("/api").expect(200);
+  });
+  it("GET responds with all apis objects", () => {
     return request(app)
-    .get("/api/doesNotExist")
-    .expect(404)
-  })
-})
+      .get("/api")
+      .then((response) => {
+        expect(typeof response.body.apis).toEqual("object");
+        expect(response.body.apis["GET /api"].description).toBe(
+          "serves up a json representation of all the available endpoints of the api"
+        );
+      });
+  });
+});
--- models/topicsModel.js
+++ models/topicsModel.js
@@ -1,7 +1,16 @@
 const db = require("../db/connection");
+const fs = require("fs/promises")
+
 
 exports.getAllTopics = () =>{
     return db.query("SELECT * FROM topics").then((topics)=>{
         return topics.rows
     })
+}
+
+exports.getAllApis = () =>{
+    return fs.readFile("./endpoints.json", "utf-8").then((apis)=>{
+        return JSON.parse(apis)
+    })
+    
 }
