93266d54bccd664143b683cfe3c81f4babc18f11
jacob <jacobabgarner@gmail.com>
2023-11-23 16:42:27 +0000

    adding task 12 and test

--- models/topicsModel.js
+++ models/topicsModel.js
@@ -26,7 +26,24 @@ exports.getAllApis = () => {
 
 exports.getArticleById = (id) => {
   return db
-    .query(`SELECT * FROM articles WHERE article_id = $1`, [id])
+    .query(
+      `SELECT 
+      articles.article_id, 
+      articles.title, 
+      articles.topic, 
+      articles.author, 
+      articles.body, 
+      articles.created_at, 
+      articles.votes, 
+      articles.article_img_url, 
+      CAST(COUNT(comments.comment_id) AS INT) AS comment_count
+    FROM articles
+    LEFT JOIN comments ON comments.article_id = articles.article_id
+    WHERE articles.article_id = $1
+    GROUP BY articles.article_id
+  `,
+      [id]
+    )
     .then((article) => {
       if (article.rows.length === 0) {
         return Promise.reject({ status: 404, msg: "Article does not exist" });
